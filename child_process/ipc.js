8530e59943742c71b0ec5280968347327efc684c
Jason <374632897@qq.com>
2017-12-09 12:24:49 +0800

    json object

--- child_process/ipc.js
+++ child_process/ipc.js
@@ -2,11 +2,13 @@ const { spawn, log } = require('./main')
 
 process.on('message', log('PROCESS_REVEIVED_MESSAGE: \n'));
 
-const subprocess = spawn('find', ['/tmp/testNpm', '-iname', '*.json', '-maxdepth', '1'], {
+// const subprocess = spawn('find', ['/tmp/testNpm', '-iname', '*.json', '-maxdepth', '1'], {
+const subprocess = spawn('echo "{\\"name\\": 123 }"', {
   // 标准输出参数需要是 json, 而这里是文件名， 以 / 开头， 所以报错了
   stdio: ['pipe', 'ipc', 'inherit'],
+  shell: true,
 });
 subprocess.on('message', log('SUBPROCESS_SEND_MESSAGE: \n'));
 subprocess.on('data', (data) => {
-  subprocess.send({ data: data.toString() })
+  subprocess.send({ data: data.valueOf() })
 });
